946d1b1c49395b2db486089f034f320a8bb1dec3
Ryzii <Ryzii@yandex.ru>
2015-07-12 10:48:12 +0300

    change webhook por

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     bot_url : "https://api.telegram.org/bot121420036:AAEwbN-DVWLLkBJgtFpfGOMdQxmtkUg03Xo/",
     server_url : "https://safe-taiga-8814.herokuapp.com",
-    server_port : 8081
+    server_port : 8443
 };
