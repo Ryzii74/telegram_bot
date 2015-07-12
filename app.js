14642027e39fcc4ea0250878b0e35bd79eacd712
Ryzii <Ryzii@yandex.ru>
2015-07-12 10:47:06 +0300

    Start webhook

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 var utils = require('./utils');
+var config = require('./config');
+
 utils.callMethod({method : 'getMe'}, function (err, data) {
-    /*utils.callMethod({
-        method : 'getUpdates',
-        form : {
-            offset : 0
-        }
-    }, function (err, data) {
-        console.log(data);
-    })*/
-    utils.callMethod({method : 'unsetWebhook', form : {url : 'https://ya1.ru'}}, function (err, data) {
+    utils.callMethod({method : 'setWebhook', form : {url : config.server_url + ':' + config.server_port + '/'}}, function (err, data) {
         console.log(data);
     })
 });
+
+var http = require('http');
+function onRequest(request, responder) {
+    var data = '';
+    request.on('data', function(chunk){
+        data += chunk.toString();
+    });
+    request.on('end', function () {
+        console.log(request.url);
+        console.log(data);
+    });
+}
+http.createServer(onRequest).listen(config.server_port);
+
--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@ var config = require('./config');
 var request = require('request');
 
 module.exports.callMethod  = function (data, callback) {
-    request({url : config.url + data.method, form : data.form || {}}, function (err, response, body) {
+    request({url : config.bot_url + data.method, form : data.form || {}}, function (err, response, body) {
         var json = JSON.parse(body);
         if (err || response.statusCode != 200 || !json || !json.ok) {
             console.log('error in response ' + data.method);
