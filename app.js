74428068d6749318c673c651e886318776a66ef7
Ryzii <Ryzii@yandex.ru>
2015-07-12 11:02:02 +0300

    Remove standart server

--- app.js
+++ app.js
@@ -20,10 +20,10 @@ function onRequest(request, responder) {
 }
 http.createServer(onRequest).listen(config.server_port);
 
-function standartRequest(request, responder) {
+/*function standartRequest(request, responder) {
     responder.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
     responder.write("Все правильно работает!");
     responder.end();
 }
-http.createServer(standartRequest).listen(81);
+http.createServer(standartRequest).listen(81);*/
 
