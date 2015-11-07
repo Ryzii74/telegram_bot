87a4c29fe76659cf7b5bf384167f026cfc7ac66b
Dmitri Boronin <Ryzii@yandex.ru>
2015-11-07 16:28:08 +0500

    add checking players for private messages

--- app.js
+++ app.js
@@ -15,109 +15,93 @@ function makeRequest() {
         }
     }, function (err, data) {
         if (err || !data.ok) {
-            err && console.log(err);
-            data && console.log(data);
-
-            return makeRequest();
+            err && console.log('telegram request error', err);
+            data && console.log('telegram request data', data);
+        } else {
+            data.result.forEach(function (message) {
+                newMessage(message.message);
+                offset = message.update_id + 1;
+            });
         }
-
-        var messages = data.result;
-        messages.forEach(function (message) {
-            newMessage(message.message);
-            offset = message.update_id + 1;
-        });
 
         makeRequest();
     });
 }
 
 function newMessage(message) {
-    var result = {
-        isPrivate: false
-    };
+    if (message.from.id === message.chat.id) return gotPrivateMessage(message);
+    gotChatMessage(message);
+}
 
-    if (message.from.id === message.chat.id) {
-        try {
-            if (message.text[0] && message.text[0] === '/') {
-                require(config.path_to_commands + message.text)((message.text === '/getid') ? message.chat.id : message.text, function (text) {
-                    if (!text) return;
-                    utils.callMethod({
-                        method: 'sendMessage',
-                        form: {
-                            chat_id: message.chat.id,
-                            text: text,
-                            reply_to_message_id: message.message_id
-                        }
-                    });
-                });
-            } else {
-                require(config.path_to_private_commands + 'code')(message.text, function (text) {
-                    utils.callMethod({
-                        method: 'forwardMessage',
-                        form: {
-                            chat_id: config.game.chat_id,
-                            from_chat_id: message.chat.id,
-                            message_id: message.message_id
-                        }
-                    }, function () {
-                        if (!text) return;
-                        utils.callMethod({
-                            method: 'sendMessage',
-                            form: {
-                                chat_id: config.game.chat_id,
-                                text: text,
-                                reply_to_message_id: message.message_id
-                            }
-                        });
-                    });
+function gotChatMessage(message) {
+    if (!message.text) return;
 
-                    if (!text) return;
-                    utils.callMethod({
-                        method: 'sendMessage',
-                        form: {
-                            chat_id: message.chat.id,
-                            text: text,
-                            reply_to_message_id: message.message_id
-                        }
-                    });
-                });
-            }
+    message.command = message.text.split(' ')[0];
+    message.args = message.text.replace(message.command + ' ', '').replace(message.command, '');
+
+    try {
+        require(config.path_to_commands + message.command)(message.args, function (text) {
+            sendMessage(message.chat.id, text, message.message_id);
+        });
+    }
+    catch (e) {
+        console.log('<- ERROR START ------------------------------------------------------------------------------>');
+        console.log(e.message);
+        console.log(e.stack);
+        console.log('<- ERROR END -------------------------------------------------------------------------------->');
+    }
+}
+
+function sendMessage(chat_id, text, message_reply_id) {
+    if (!text) return;
+    utils.callMethod({
+        method: 'sendMessage',
+        form: {
+            chat_id: chat_id,
+            text: text,
+            reply_to_message_id: message_reply_id
         }
-        catch (e) {
-            console.log('<- ERROR START ------------------------------------------------------------------------------>');
-            console.log(e.message);
-            console.log(e.stack);
-            console.log('<- ERROR END -------------------------------------------------------------------------------->');
-        }
+    });
+}
+
+function gotPrivateMessage(message) {
+    if (config.game.players.indexOf(message.from.id) === -1
+        && config.game.players.indexOf(message.from.username) === -1
+        && message.text !== '/getid') {
+        console.log('-------------------------------------');
+        console.log('bad player try to send message to bot');
+        console.log(message);
+        console.log('-------------------------------------');
         return;
     }
 
-    if (message.text) {
-        console.log(message.text);
-        result.text = (result.isPrivate) ? message.text : message.text.replace(config.bot.name + ' ', '');
-
-        message.command = message.text.split(' ')[0];
-        message.args = message.text.replace(message.command + ' ', '').replace(message.command, '');
-
-        try {
-            require(config.path_to_commands + message.command)(message.args, function (text) {
-                text && utils.callMethod({
-                    method: 'sendMessage',
+    try {
+        if (message.text[0] && message.text[0] === '/') {
+            require(config.path_to_commands + message.text)(
+                (message.text === '/getid') ? message.chat.id : message.text, function (text) {
+                sendMessage(message.chat.id, text, message.message_id);
+            });
+        } else {
+            require(config.path_to_commands + 'code')(message.text, function (text) {
+                utils.callMethod({
+                    method: 'forwardMessage',
                     form: {
-                        chat_id: message.chat.id,
-                        text: text,
-                        reply_to_message_id: message.message_id
+                        chat_id: config.game.chat_id,
+                        from_chat_id: message.chat.id,
+                        message_id: message.message_id
                     }
+                }, function () {
+                    sendMessage(config.game.chat_id, text, message.message_id);
                 });
+
+                sendMessage(message.chat.id, text, message.message_id);
             });
         }
-        catch (e) {
-            console.log('<- ERROR START ------------------------------------------------------------------------------>');
-            console.log(e.message);
-            console.log(e.stack);
-            console.log('<- ERROR END -------------------------------------------------------------------------------->');
-        }
-    } else {
-        console.log('Сообщение не текстовое');
+    }
+    catch (e) {
+        console.log('<- ERROR START ------------------------------------------------------------------------------>');
+        console.log(e.message);
+        console.log(e.stack);
+        console.log('<- ERROR END -------------------------------------------------------------------------------->');
     }
 }
--- config.js
+++ config.js
@@ -1,3 +1,12 @@
+var playersList = {
+    ryzii : 59745477,
+    kiseniya : 'Kiseniya',
+    vental : 63910101,
+    sloyArtur : 51822055,
+    sloyRuslan : 'Grizzly174',
+    sloyNikita : 55787756
+};
+
 module.exports = {
     bot : {
         url : "https://api.telegram.org/bot115498454:AAFPBBUy6Em-8DkDfp38UfBuH7sP28v6vV8/",
@@ -9,12 +18,19 @@ module.exports = {
         url : 'http://chel.en.cx/gameengines/encounter/play/50948',
         host : 'chel.en.cx',
         //chat_id : "-33965431",
-        chat_id : "-31988268",
+        chat_id : "-33965431",
         //referer : "http://demo.en.cx/gameengines/encounter/play/23373",
         referer : "http://chel.en.cx/GameDetails.aspx?gid=50948",
         cookie :   "GUID=f6af7916%2D561b%2D460f%2D9d3a%2Db2773918ff10; __utmt_t0=1; check=1; atoken=uid%3d1416666%26iss%3d0%26iscd%3d1%26tkn%3d719387eda0d25411c161267649c5b0584c8345ef; _ym_visorc_11409715=w; lang=ru; __utma=197665149.1157910272.1420107378.1437196256.1437199161.83; __utmb=197665149.12.10.1437199161; __utmc=197665149; __utmz=197665149.1434693275.71.9.utmcsr=vk.com|utmccn=(referral)|utmcmd=referral|utmcct=/en454000; Domain=chel%2Een%2Ecx; stoken=v4j54pyyxbzuhqypseryyddv",
         //cookie : "GUID=f6af7916%2D561b%2D460f%2D9d3a%2Db2773918ff10; __utma=142023398.1745113043.1436693934.1436693934.1436693934.1; __utmc=142023398; __utmz=142023398.1436693934.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmt_t0=1; check=1; atoken=uid%3d181101%26iss%3d0%26iscd%3d1%26tkn%3db71f0c7459d1f31483ff37183c1cf17536c8f3fb; lang=ru; __utma=197665149.1157910272.1420107378.1437196256.1437199161.83; __utmb=197665149.7.10.1437199161; __utmc=197665149; __utmz=197665149.1434693275.71.9.utmcsr=vk.com|utmccn=(referral)|utmcmd=referral|utmcct=/en454000; _ym_visorc_11409715=w; Domain=chel%2Een%2Ecx; stoken=v4j54pyyxbzuhqypseryyddv",
-        players : ["21544566"]
+        players : [
+            playersList.kiseniya,
+            playersList.ryzii,
+            playersList.vental,
+            playersList.sloyRuslan,
+            playersList.sloyNikita,
+            playersList.sloyArtur
+        ]
     },
 
     path_to_commands : './commands/',
