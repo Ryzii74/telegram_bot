9943d9e796af9cf2fd65801fac9f22143ed9acf6
Dmitri Boronin <Ryzii@yandex.ru>
2015-12-05 14:24:07 +0500

    FIX change config aut

--- commands/gamelogin.js
+++ commands/gamelogin.js
@@ -1,7 +1,7 @@
 module.exports = function (params, callback, senderId) {
     if (senderId !== global.config.ownerId) return callback('Для вас недоступна данная команда!');
     if (!global.config.logins.hasOwnProperty(params)) return callback('Плохой выбор для регистрации, чувак!');
-    global.config.game.auth.login = global.config.logins[params].login;
-    global.config.game.auth.password = global.config.logins[params].password;
+    global.config.game.auth.Login = global.config.logins[params].login;
+    global.config.game.auth.Password = global.config.logins[params].password;
     callback(`Готово! Логин для входа в игру - ${params}`);
 };
