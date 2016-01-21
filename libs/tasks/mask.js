b424d61eeefe8277aa64c33f5ce9a46f3fb0e93b
Dmitri Boronin <Ryzii@yandex.ru>
2016-01-21 22:57:06 +0500

    FIX replacing in mask

--- libs/tasks/mask.js
+++ libs/tasks/mask.js
@@ -1,5 +1,5 @@
 module.exports.exec = function (text) {
-    var reg = new RegExp(text.replace('*', '\\S*').replace('?', '\\S'), 'gi');
+    var reg = new RegExp('^' + text.replace(/\*/g, '\\S*').replace(/\?/g, '\\S') + '$', 'gi');
 
     return global.WORDS_ARRAY.filter(function (word) {
         return reg.exec(word);
