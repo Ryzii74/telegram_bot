359b3737dd9f2617083ae8a3a8b1cc078ff3d15d
Dmitri Boronin <Ryzii@yandex.ru>
2016-02-28 23:17:18 +0500

    ADD help commands

---
+++ commands/help.js
@@ -0,0 +1,17 @@
+module.exports = function (params, callback) {
+    var text = '';
+
+    text += 'task - задание';
+    text += '\nhints - информация о подсказках';
+    text += '\ntime - оставшееся время на уровень';
+    text += '\nstarttime - время до начала игры';
+    text += '\nbonus - бонусы на уровне';
+    text += '\nfoundbonus - подсказки с уже введенных бонусных кодов';
+    text += '\nc (rus/en), code, просто "/" - вбить код';
+    text += '\ncodes - информация по кодам';
+    text += '\nsectors - полный список секторов';
+    text += '\nhelp - помощь по командам движка';
+    text += '\nhelpsolve - помощь по вспомогательным командам';
+
+    callback(text);
+};
---
+++ commands/helpsolve.js
@@ -0,0 +1,10 @@
+module.exports = function (params, callback) {
+    var text = '';
+
+    text += 'an / anagramma - анаграмма';
+    text += '\nchlen - расчлененки';
+    text += '\nцезарь - шифр цезаря';
+    text += '\nmask - поиск по маске';
+
+    callback(text);
+};
