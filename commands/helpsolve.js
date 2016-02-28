f15b26fdf25251ac8e5f402bbcf85dc5d903d3dd
Dmitri Boronin <Ryzii@yandex.ru>
2016-02-28 23:20:21 +0500

    ADD solve help inf

--- commands/helpsolve.js
+++ commands/helpsolve.js
@@ -1,10 +1,10 @@
 module.exports = function (params, callback) {
     var text = '';
 
-    text += 'an / anagramma - анаграмма';
-    text += '\nchlen - расчлененки';
-    text += '\nцезарь - шифр цезаря';
-    text += '\nmask - поиск по маске';
+    text += 'an / anagramma - анаграмма (* - любая буква)';
+    text += '\nchlen - расчлененки ( слово кол-воБукв слово кол-воБукв)';
+    text += '\nцезарь - шифр цезаря (фраза)';
+    text += '\nmask - поиск по маске (? - одна буква, * - любое количество букв)';
 
     callback(text);
 };
