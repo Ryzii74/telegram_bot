1d96ee4888385d2c54b3dbf2ccb806da2634b34b
Dmitri Boronin <Ryzii@yandex.ru>
2016-02-28 18:40:05 +0500

    CHANGE game config getting

--- commands/gameconfig.js
+++ commands/gameconfig.js
@@ -1,5 +1,10 @@
 module.exports = function (params, callback, senderId) {
     if (senderId !== global.config.ownerId) return callback('Для вас недоступна данная команда!');
-    var config = JSON.parse(JSON.stringify(global.config.game));
-    callback(JSON.stringify(config))
+    var result = '';
+    result += `Логин - ${global.config.choosedLogin}`;
+    result += `Чат - ${global.config.choosedChat}`;
+    result += `Id игры - ${global.config.game.id}`;
+    result += `Host игры - ${global.config.game.host}`;
+
+    callback(result);
 };
