8e9b51cb6ea8b47a8cf752467f12a3309c6c1f69
Dmitri Boronin <Ryzii@yandex.ru>
2016-02-29 20:21:23 +0500

    FIX fuck you

--- commands/fuckyou.js
+++ commands/fuckyou.js
@@ -2,5 +2,7 @@ module.exports = function (params, callback, senderId) {
     if (senderId !== global.config.ownerId) return callback('Для вас недоступна данная команда!');
 
     callback('Сам дурак!');
-    process.exit(0);
+    setTimeout(function () {
+        process.exit(0);
+    }, 5000);
 };
