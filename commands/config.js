d0070dceba259e8a281b79194f067fa5b27a58f0
Dmitri Boronin <Ryzii@yandex.ru>
2016-03-02 20:21:26 +0500

    REMOVE limit for only owner on config

--- commands/config.js
+++ commands/config.js
@@ -1,7 +1,5 @@
 module.exports = function (params, callback, senderId) {
-    if (senderId !== global.config.ownerId) return callback('Для вас недоступна данная команда!');
-    var result = '';
-    result += `Логин - ${global.config.choosedLogin}\n`;
+    var result = `Логин - ${global.config.choosedLogin}\n`;
     result += `Чат - ${global.config.choosedChat}\n`;
     result += `Игра - http://${global.config.game.host}/GameDetails.aspx?gid=${global.config.game.id}`;
 
