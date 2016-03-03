4a835c596577a7bf18031e893966cddc4a7158de
Dmitri Boronin <Ryzii@yandex.ru>
2016-03-04 00:11:32 +0500

    FIX anekdot <br>

--- commands/anekdot.js
+++ commands/anekdot.js
@@ -8,9 +8,9 @@ module.exports = function (params, callback) {
             Referer : "http://www.anekdots.com/%D0%A1%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D1%8B%D0%B9_%D0%B0%D0%BD%D0%B5%D0%BA%D0%B4%D0%BE%D1%82/"
         }
     }, function (err, response, body) {
-        console.log(err, body);
         if (err || !body) return callback(textError);
 
+        body = body.replace('\n');
         var $ = require('cheerio').load(body);
         callback($('.marg10').text() || textError);
     });
