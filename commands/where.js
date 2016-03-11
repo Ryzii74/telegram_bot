1e42561de90444a4353b2705fac6ebd109d909da
Dmitri Boronin <Ryzii@yandex.ru>
2016-03-11 21:35:19 +0500

    ADD where command

--- commands/where.js
+++ commands/where.js
@@ -1,3 +1,7 @@
 module.exports = function (params, callback) {
-    callback('test');
+    require('../libs/where').getLastPoint((err, data) => {
+        if (err) return callback(err);
+
+        callback(`http://static-maps.yandex.ru/1.x/?lang=en-US&ll=${data.lng},${data.lat}&z=13&l=map&size=600,300&pt=${data.lng},${data.lat},flag`);
+    });
 };
