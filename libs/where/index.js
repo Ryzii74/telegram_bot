a689ea7e8e4df6e9133066cbcb0ea1f51469ef11
Dmitri Boronin <Ryzii@yandex.ru>
2016-03-11 21:35:07 +0500

    ADD where lib

---
+++ libs/where/index.js
@@ -0,0 +1,31 @@
+module.exports.getLastPoint = getLastPoint;
+function getLastPoint(callback) {
+    require('request').get('http://46.101.140.237:3000/points/getLast/test', function (err, response, data) {
+        if (err) return callback('Ошибка получения координат');
+
+        try {
+            data = JSON.parse(data);
+            if (!data.success) {
+                console.log(data.error);
+                return callback("Сервис ответил ошибкой")
+            }
+
+            callback(null, data.data);
+        }
+        catch (e) {
+            callback("Данные какие-то странные");
+        }
+    });
+}
+
+module.exports.findCoordinates = function (text) {
+    return false;
+};
+
+module.exports.getRoute = function (coordinates, callback) {
+    getLastPoint((err, data) => {
+        if (err) return callback("Ошибка построения пути!");
+
+        callback(null, `http://static-maps.yandex.ru/1.x/?lang=en-US&ll=${data.lng},${data.lat}&z=13&l=map&size=600,300&pt=${data.lng},${data.lat},flag`);
+    });
+};
