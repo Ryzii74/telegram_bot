a04f1bdbf374815dc8922ed173053b43d67bb577
Dmitri Boronin <Ryzii@yandex.ru>
2016-03-11 21:42:32 +0500

    ADD auto positioning of the map

--- libs/where/index.js
+++ libs/where/index.js
@@ -26,6 +26,6 @@ module.exports.getRoute = function (coordinates, callback) {
     getLastPoint((err, data) => {
         if (err) return callback("Ошибка построения пути!");
 
-        callback(null, `http://static-maps.yandex.ru/1.x/?lang=en-US&ll=${data.lng},${data.lat}&z=13&l=map&size=600,300&pt=${data.lng},${data.lat},flag~${coordinates.lng},${coordinates.lon},flag`);
+        callback(null, `http://static-maps.yandex.ru/1.x/?lang=en-US&l=map&size=600,400&pt=${data.lng},${data.lat},flag~${coordinates.lng},${coordinates.lon},flag`);
     });
 };
